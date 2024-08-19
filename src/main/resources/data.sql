0bc97e977d05128c61b404fd68360a3c2fe8aa1e
injae-348 <ant0593@naver.com>
2024-08-19 22:16:10 +0900

    test: test용 data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO MENU (name, price, category, image_name) VALUES
+-- 커피 카테고리
+('아메리카노', 4000, '커피', 'americano.png'),
+('카페라떼', 4500, '커피', 'caffelatte.png'),
+('연유라떼', 5000, '커피', 'condensedmilk_latte.png'),
+('바닐라라떼', 4800, '커피', 'vanilla_latte.png'),
+('카푸치노', 4500, '커피', 'cappuccino.png'),
+('카라멜마끼야또', 5000, '커피', 'caramel_macchiato.png'),
+('카페모카', 4700, '커피', 'caffe_mocha.png'),
+('아인슈페너', 5500, '커피', 'eiskaffee.png'),
+
+-- 차 카테고리
+('복숭아티', 3500, '차', 'peach_tea.png'),
+('리치캐모마일', 4000, '차', 'lychee_camomile.png'),
+('청귤얼그레이', 3800, '차', 'green_citrus_earlgrey.png'),
+('트리플민트', 3600, '차', 'triple_mint.png'),
+('애플히비스커스', 3700, '차', 'apple_hibiscus.png'),
+('허나유자티', 3900, '차', 'honey_yuja_tea.png'),
+('허니자몽티', 4000, '차', 'honey_persimmon_tea.png'),
+('허니레몬티', 3800, '차', 'honey_lemon_tea.png'),
+
+-- 에이드 카테고리
+('레몬에이드', 4000, '에이드', 'lemonade.png'),
+('하비스커스에이드', 4200, '에이드', 'hibiscus_aid.png'),
+('블루베리에이드', 4500, '에이드', 'blueberry_aid.png'),
+('자몽에이드', 4300, '에이드', 'grapefruit_aid.png');
